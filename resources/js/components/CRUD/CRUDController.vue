ae7d47b32d721b33e3eb8d613f521bfdb4634dd2
unknown <patel.udip@gmail.com>
2019-08-06 21:47:46 -0400

    add empty files, need to split CRUDPanel into View and Controller and use vue props and events instead of $refs

---
+++ resources/js/components/CRUD/CRUDController.vue
@@ -0,0 +1,20 @@
+<!-- CRUD Operations Controller, a pop-up modal that can dynamically display and submit varying forms -->
+<template>
+    <div id="CRUDModal" class="modal fade" data-backdrop="static" data-keyboard="false">
+        <div class='modal-dialog'>
+            <div class='modal-content'>
+                <form :id="formID" enctype="multipart/form-data">
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
---
+++ resources/js/components/CRUD/CRUDView.vue
@@ -0,0 +1,4 @@
+<!-- the admin dashboard view, can list all data here and trigger operations from CRUDController -->
+<template>
+
+</template>
